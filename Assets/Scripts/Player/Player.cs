b0f379f260cfd3bf80fd30fe6ab5f93343e4015a
Hank <henry.vigor@gmail.com>
2023-10-17 17:52:44 -0500

    Added Player.cs for public access to player GameObject and making sure only one player exists at a time

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// Player
+/// <summary>
+///     Singleton <see cref="MonoBehaviour"/> class that ensures only one player
+///     object is loaded
+/// </summary>
+public class Player : MonoBehaviour {
+    /// <summary>Pointer to player <see cref="GameObject"/></summary>
+    /// <value>Set on <see cref="Instance"/> set</value>
+    public static GameObject Obj { get; private set; }
+    
+    static Player _Instance;
+    /// <summary>Singleton <see cref="Player"/> instance</summary>
+    /// <value>Set if null</value>
+    static Player Instance {
+        get => _Instance;
+        set {
+            if (_Instance == null) {
+                _Instance = value;
+                Obj = value.gameObject;
+            }
+        }
+    }
+    
+    void Awake() {
+        Instance = this;
+        if (Instance != this) Destroy(gameObject);
+        else DontDestroyOnLoad(gameObject);
+    }
+}

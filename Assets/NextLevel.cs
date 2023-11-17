59f735e09c814826a5fbae36a952fb22b956991e
OrangeHundred <rwb27@live.missouristate.edu>
2023-11-17 00:28:44 -0600

    Fixing potential merge conflicts with shop branch

--- Assets/NextLevel.cs
+++ Assets/NextLevel.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NextLevel : MonoBehaviour
 {
@@ -12,14 +13,25 @@ public class NextLevel : MonoBehaviour
         levelIndicator = FindObjectOfType<LevelIndicator>();
     }
 
+    const int shopFrequency = 3; // How often a shop will appear
+    const int shopScene = 1; // Name of shop scene   
+
     void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject == Player.Obj && cavernGenerator != null)
         {
-            Destroy(GameObject.Find("BlockHolder"));
-            cavernGenerator.GenerateCavern();
-            Destroy(gameObject);
+            if (levelIndicator.LevelValue % shopFrequency == 0)
+            {
+                SceneManager.LoadScene(shopScene);
+            }
+            else
+            {
+                Destroy(GameObject.Find("BlockHolder"));
+                cavernGenerator.GenerateCavern();
+            }
+            
             levelIndicator.LevelValue += 1; // Increment the level value, this will automatically update the UI text
+            Destroy(gameObject);
         }
     }
 }

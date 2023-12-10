ed0a9be935f237a470ebd820aef810ffd19e4738
ctannlund <conortannlund@gmail.com>
2023-12-10 14:43:32 -0600

    Removed unneccesary commented out code in NextLevel

--- Assets/NextLevel.cs
+++ Assets/NextLevel.cs
@@ -6,25 +6,11 @@ using UnityEngine.SceneManagement;
 public class NextLevel : MonoBehaviour, IEducational
 {
     public CavernGenerator cavernGenerator; // Reference to the CavernGenerator component
-    //private SpriteRenderer cavernSpriteRenderer; // Reference to the Sprite Renderer of the cavern prefab
     private LevelIndicator levelIndicator; // Declare levelIndicator at the class level
-    //public GameObject trappedStoneBlock; // Reference to trap
-
-    //private Color[] predefinedColors = new Color[]
-    //{
-    //new Color(141f / 255f, 129f / 255f, 154f / 255f),   // Dark-blue
-    //new Color(215f / 255f, 195f / 255f, 114f / 255f),   // Yellowish
-    //new Color(214f / 255f, 154f / 255f, 114f / 255f),   // Orange-ish
-    //new Color(164f / 255f, 147f / 255f, 173f / 255f)   // Purple-ish
-    //    // Add more predefined colors as needed
-    //};
 
     void Start()
     {
         levelIndicator = FindObjectOfType<LevelIndicator>();
-
-        // Find the SpriteRenderer of the cavern prefab in generator script
-        //cavernSpriteRenderer = cavernGenerator.cavernSpritePrefab.GetComponent<SpriteRenderer>();
     }
 
     const int shopFrequency = 3; // How often a shop will appear
